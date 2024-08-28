f6f0a45d67811c421756db1b52866a390473477f
lukathao <lukayeethao@gmail.com>
2024-08-27 19:49:08 -0500

    static site ready

--- src/app/static/chubbys/page.tsx
+++ src/app/static/chubbys/page.tsx
@@ -1,59 +1,18 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Chubbys = () => {
+  const imgWidth = "2400";
+  const imgHeight = "5000";
+
   return (
+    
     <>
-      <div>Chubbys</div>
       <div>
-        <h1>Mains</h1>
-        <div>
-          <div>
-            <div><h2>Tikka Masala</h2></div><div><h2>18</h2></div>
-          </div>
-          <div>
-            <p>Marinated chicken breast simmered with potatoes, carrots, and edamame in our masala sauce. Serviced with jasmine rice.</p>
-          </div>
-        </div>
-        <div>
-          <div>
-            <div><h2>Chicken Fried Rice</h2></div><div><h2>16</h2></div>
-          </div>
-          <div>
-            <p>Chicken breast, carrots, pease, onions, and of course rice. Pan fried on high heat.</p>
-          </div>
-        </div>
-        <div>
-          <div>
-            <div><h2>Lollipop Drumsticks</h2></div><div><h2>18</h2></div>
-          </div>
-          <div>
-            <p>Three jumbo chicken drumsticks beer battered and deep fried. Served with mash potatoes and candied carrots.</p>
-          </div>
-        </div>
-        <div>
-          <div>
-            <div><h2>Thai Curry Mash</h2></div><div><h2>18</h2></div>
-          </div>
-          <div>
-            <p>A low spice level Thai curry with carrots, broccoli, mushrooms, onions, and chicken. Served over mashed potatoes! Option of jasmin rice instead of mashed potatoes also available.</p>
-          </div>
-        </div>
-        <div>
-          <div>
-            <div><h2>Chicken Tenders</h2></div><div><h2>18</h2></div>
-          </div>
-          <div>
-            <p>Five chicken tenders served with beer battered fries and your choice of dipping sauce.</p>
-          </div>
-        </div>
-        <div>
-          <div>
-            <div><h2>Chicken Tenders - kids</h2></div><div><h2>9</h2></div>
-          </div>
-          <div>
-            <p>Any child 12 or under. Comes with three chicken tenders, fries, and a juice box.</p>
-          </div>
-        </div>
+        <Image src="/menu1.jpg" alt="menu1" width={imgWidth}  height={imgHeight} />
+        <Image src="/menu2.jpg" alt="menu2" width={imgWidth}  height={imgHeight} />
+        <Image src="/menu3.jpg" alt="menu3" width={imgWidth}  height={imgHeight} />
+        <Image src="/menu4.jpg" alt="menu4" width={imgWidth}  height={imgHeight} />
       </div>
     </>
     
