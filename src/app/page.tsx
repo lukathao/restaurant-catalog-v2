f4085cf476195a212ed4780ba45a216a672ef9ce
lukathao <lukayeethao@gmail.com>
2024-08-27 19:57:42 -0500

    fix errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href="/static/chubbys">Chubby's Asian Fusion Cuisine</Link>
+      <Link href="/static/chubbys">Chubby&apos;s Asian Fusion Cuisine</Link>
     </main>
   );
 }
