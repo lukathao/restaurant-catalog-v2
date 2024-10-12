9bf66d6ddb9c7cdd861eb9608b99596f0bc1c36f
lukathao <lukayeethao@gmail.com>
2024-10-11 23:17:26 -0500

    moved form to server

--- src/app/restaurants/add/page.tsx
+++ src/app/restaurants/add/page.tsx
@@ -1,43 +1,16 @@
 "use client";
 
 import React from 'react';
-import { useForm } from 'react-hook-form';
-import { useState } from 'react';
+import { useFormState } from 'react-dom';
+import { createBusiness } from '@/app/service/Restaurant.service';
+import { SaveButton } from '@/app/components/buttons/buttons';
+
 
 const AddRestaurant = () => {
-  type BusinessType = {
-    business_name: string;
-    business_owner: string;
-    email: string,
-  }
-  const { register, handleSubmit, formState: { errors, isSubmitting }, reset } = useForm<BusinessType>({
-    reValidateMode: "onSubmit",
-  });
-  const [loading, setLoading] = useState(false);
-
-  const submit = async (business: BusinessType) => {
-    try {
-      const response = await fetch('/api/restaurants/add', {
-        headers: {
-          Accept: "application/json",
-        },
-        method: "POST",
-        body: JSON.stringify(business),
-      });
-      if (response) {
-        // TODO create confirmation popup with message
-        const data = await response.json();
-        console.log("Submitted Data");
-        console.log(data);
-      };
-      // TODO reset only if successful reservation
-      reset();
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
+  const initialState = {
+    message: "Enter restaurant information.",
   };
+  const [state, formAction] = useFormState(createBusiness, initialState);
 
   return (
     <>
@@ -46,59 +19,43 @@ const AddRestaurant = () => {
         <p>Add a new restaurant to the catalog</p>
         <form
           className='min-w-96 mx-auto space-y-6 p-4 my-4 justify-center items-center'
-          onSubmit={handleSubmit(submit)}>
+          action={formAction}>
 
-          <div id="business_name">
+          <div>
             <label className='font-semibold'>Business name</label>
             <input
               type="text"
               className="text-black border-gray-400 border-2 block px-4 w-full rounded"
-              {...register("business_name", {
-                required: "Invalid business name",
-                minLength: {
-                  value: 1,
-                  message: "Name is too short"
-                }
-              })} />
-            {errors.business_name && (<p className="text-red-500">{errors.business_name.message}</p>)}
+              id="business_name"
+              name="business_name"
+            />
           </div>
 
-          <div id="business_owner">
+          <div>
             <label className='font-semibold'>Owner name</label>
             <input
               type="text"
               className="text-black border-gray-400 border-2 block px-4 w-full rounded"
-              {...register("business_owner", {
-                required: "Invalid business owner name",
-                minLength: {
-                  value: 1,
-                  message: "Name is too short"
-                }
-              })} />
-            {errors.business_owner && (<p className="text-red-500">{errors.business_owner.message}</p>)}
+              id="business_owner"
+              name="business_owner"
+            />
           </div>
 
-          <div id="email">
+          <div>
             <label className='font-semibold'>Contact email:</label>
             <input
               type="text"
               className="text-black border-gray-400 border-2 block px-4 w-full rounded"
-              {...register("email", {
-                required: "Invalid email",
-                minLength: {
-                  value: 3,
-                  message: "Email is too short"
-                }
-              })} />
-            {errors.email && (<p className="text-red-500">{errors.email.message}</p>)}
+              id="email"
+              name="email"
+            />
           </div>
           <div>
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded border-black disabled:opacity-50 disabled:cursor-not-allowed"
-              disabled={isSubmitting}>
-              Submit
-            </button>
+            <SaveButton />
           </div>
+          <p aria-live='polite' className='sr-only' role='status'>
+            {state?.message}
+          </p>
         </form>
       </div>
     </>
--- src/app/service/Restaurant.service.ts
+++ src/app/service/Restaurant.service.ts
@@ -1,6 +1,8 @@
 "use server";
 
 import { RestaurantsData } from "@/interfaces/Restaurant.interfaces";
+import { revalidatePath } from "next/cache";
+import { z } from 'zod';
 
 export async function getBusinesses() {
   let businesses: RestaurantsData[] = [];
@@ -24,7 +26,40 @@ export async function getRestaurantInformation(id: string) {
   }
 }
 
+const schema = z.object({
+  business_name: z.string().min(1),
+  business_owner: z.string().min(1),
+  email: z.string().min(3),
+});
+
+export async function createBusiness(prevState: any, formData: FormData) {
+  try {
+    const data = schema.parse({
+      business_name: formData.get("business_name"),
+      business_owner: formData.get("business_owner"),
+      email: formData.get("email"),
+    });
+    const business: RestaurantsData = {
+      business_id: null,
+      business_name: data.business_name,
+      business_owner: data.business_owner,
+      email: data.email,
+    }
+    const result = await saveBusiness(business);
+    if (result > 0) {
+      return { message: "Created new business." }
+    } else {
+      return { message: "Unable to create new business." }
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    revalidatePath("/");
+  }
+};
+
 export async function saveBusiness(restaurantData: RestaurantsData) {
   // TODO save to golang api with grpc call
+  console.log(restaurantData);
   return 1;
 }
